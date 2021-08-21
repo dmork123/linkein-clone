73a70c9dbc5868ceb579b74bec59db1cb121880e
Davo <david.morkos@global.ntt>
2021-08-21 22:29:37 +1000

    added header component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,43 @@
 import styled from 'styled-components';
 
 const Home = (props) => {
-    return <div>Home</div>
+    return (
+        <Container>
+            <Section>
+                <h5>
+                    <a>Hiring in a hurry? - </a>
+                </h5>
+                <p>
+                    Find talented pros in record time with upwork.
+                </p>
+            </Section>
+        </Container>
+    )
 };
+
+const Container = styled.div`
+    padding-top: 52px;
+    max-width: 100%;
+`;
+
+const Content = styled.div`
+    max-width: 1128px;
+    margin-left: auto;
+    margin-right: auto;
+
+`;
+
+const Section = styled.section`
+    min-height: 50px;
+    padding: 16px 0;
+    box-sizing: content-box;
+    text-align: center;
+    text-decoration: underline;
+    display: flex;
+    justify-content: center;
+    
+    
+
+`;
 
 export default Home;
