001fffe4da5c9ae452a1325f44bbb9f9ac95d1d8
Davo <david.morkos@global.ntt>
2021-08-24 13:20:28 +1000

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,11 @@ const Home = (props) => {
                     Find talented pros in record time with upwork.
                 </p>
             </Section>
+            <Layout>
+                <div>Left side</div>
+                <div>Main</div>
+                <div>Right side</div>
+            </Layout>
         </Container>
     )
 };
@@ -24,7 +29,6 @@ const Content = styled.div`
     max-width: 1128px;
     margin-left: auto;
     margin-right: auto;
-
 `;
 
 const Section = styled.section`
@@ -35,8 +39,29 @@ const Section = styled.section`
     text-decoration: underline;
     display: flex;
     justify-content: center;
-    
-    
+    h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+   }
+  p {
+    padding: 22px 0;
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+    display: gird;
+    grid-template-areas: "leftside main rightside";
+    /* grid-template-columns: minmax(0,5fr) minmax(0,12fr) minmax(300px, 7fr); */
 
 `;
 
